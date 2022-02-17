26a48567ef35bbdcc198a5456594e1d73744ad56
José Manuel Caballero <gorjc87@outlook.com>
2022-02-16 19:07:32 -0700

    Solved Challenge: divideString

--- src/divideString.js
+++ src/divideString.js
@@ -6,7 +6,7 @@ Given a string of length 'N' made up of only letters 'a' and 'b', write a functi
     - Each part must contains the same number of letters 'a'.
     - N is an integer within the range [1..40,000]
     - The string S contains only the letters 'a' and 'b'.
-    - Return the number of ways the string can be divided into three parts following the prevous rules.
+    - Return the number of ways the string can be divided into three parts following the previous rules.
 
 Examples:
     Input = "babaa" => Output: 2 => Combinations: "ba | ba | a" and "bab | a | a".
@@ -24,8 +24,44 @@ Algorithm
     combination 6: bab | a | a - Valid
 */
 
-function divideString(string){
-    return -1;
+const { parseConfigFileTextToJson } = require("typescript");
+
+function divideString(string) {
+  const stringIsEmpty = string === null || string.length === 0;
+  if (stringIsEmpty) {
+    return 0;
+  }
+  let combinations = [];
+  const letters = string.split("");
+
+  for (let i = 0; i < letters.length - 2; i++) {
+    let beginPart1 = 0;
+    let incrementPart1 = i + 1;
+    let part1 = string.substr(beginPart1, incrementPart1);
+
+    for (let j = 0; j < letters.length - incrementPart1 - 1; j++) {
+      let beginPart2 = beginPart1 + incrementPart1;
+      let incrementPart2 = j + 1;
+      let part2 = string.substr(beginPart2, incrementPart2);
+
+      let beginPart3 = beginPart2 + incrementPart2;
+      let incrementPart3 = string.length - incrementPart1 - incrementPart2;
+      let part3 = string.substr(beginPart3, incrementPart3);
+
+      let lettersAinPart1 = (part1.match("a") ?? []).length;
+      let lettersAinPart2 = (part2.match("a") ?? []).length;
+      let lettersAinPart3 = (part3.match("a") ?? []).length;
+
+      let condition =
+        lettersAinPart1 === lettersAinPart2 &&
+        lettersAinPart2 === lettersAinPart3;
+
+      if (condition) {
+        combinations.push([part1, part2, part3]);
+      }
+    }
+  }
+  return combinations.length;
 }
 
 module.exports = divideString;
