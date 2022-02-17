bd6cf129ea145b47c3544cabbdffb9ffbd36e1ef
José Manuel Caballero <gorjc87@outlook.com>
2022-02-16 19:10:37 -0700

    Solved Challenge: minimalDifference

--- src/minimalDifference.js
+++ src/minimalDifference.js
@@ -8,14 +8,31 @@ Given an array of n consecutive numbers. For example:[1, 2, 3, 4, 5, 6] Write a 
 Example:
     Input: [1, 2, 3, 4, 5, 6] Output => 1
 
-Demostration:
+Demonstration:
     Combination: [1, 2, 3, 4] and [5, 6] => Difference: 10 - 11 = 1
     Combination: [1, 2, 3] and [4, 5, 6] => Difference: 6 - 15 = 9
 
 */
 
-function minimalDifference(array){
-    return -1;
+function minimalDifference(numbers) {
+  let groups = [];
+  if (numbers.length < 2) {
+    return 0;
+  }
+  for (let i = 0; i < numbers.length - 1; i++) {
+    let group1 = 0;
+    let group2 = 0;
+    numbers
+      .filter((number, index) => index <= i)
+      .map((number) => (group1 += number));
+    numbers
+      .filter((number, index) => index > i)
+      .map((number) => (group2 += number));
+    groups.push(Math.abs(group1 - group2));
+  }
+  let minimal = Math.min(...groups);
+
+  return minimal;
 }
 
 module.exports = minimalDifference;
